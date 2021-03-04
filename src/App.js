ca673996dcf9857ba793fbaee96e491ecbc1ea02
Nahuel Paez <nahuelpaez86@gmail.com>
2021-03-04 14:54:29 -0300

    Modificaciones 4 de marzo 2021

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import MenuAppBar from './Componentes/Header';
 import Footer from './Componentes/Footer';
 import Body from './Componentes/body.js';
-import './Componentes/Estilo/Style-general.css';
+import './Componentes/Estilo/app.css';
 
 const App = ()=> {
  
