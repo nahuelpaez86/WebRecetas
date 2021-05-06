6a7151edba3552236f3e1481428fece3b91de236
nahuelpaez86 <76230139+nahuelpaez86@users.noreply.github.com>
2021-05-05 21:52:33 -0300

    Update Style.js

--- src/Componentes/Paciente/pages/Formulario/Componentes/Style.js
+++ src/Componentes/Paciente/pages/Formulario/Componentes/Style.js
@@ -82,7 +82,7 @@ import { makeStyles} from '@material-ui/core/styles';
    gruposDeInputs: {
      maxWidth:'500px',
      position:'relative',
-     right:'20px',
+   
     },
     
    btnMedicacion:{
